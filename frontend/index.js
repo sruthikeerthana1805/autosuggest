fb0a78affad2bc626cabd16e784cd09b43dbf69b
sruthikeerthana1805 <24wh1a05an@bvrithyderabad.edu.in>
2026-07-04 09:16:25 +0530

    toggle changes

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,27 @@
 console.log("Hello from js");
-alert("Hello from js");
+alert("hello from js");
+const users = [
+  {
+    "name" :"Jane Doe",
+    "gender" : "Female",
+    "image" : "./images/jane.png"
+  },
+  {
+    "name" :"John Doe",
+    "gender" : "Male",
+    "image" : "./images/john.png"
+  },
+]
+let curUserId = 0;
+function toggle(){
+  if(curUserId == 0)
+    curUserId=1;
+  else
+    curUserId=0;
+  // image
+  document.getElementById("img").src=users[curUserId].image;
+  // name
+  document.getElementById("card-name").innerText=users[curUserId].name;
+  document.getElementById("card-gender").innerText = users[curUserId].gender;
+  console.log("Hello from js");
+}
